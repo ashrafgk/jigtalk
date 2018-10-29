95991c17ccb8a0697a61f146a903f0461f3a738c
Ashraf Khattab <ashrafkhattab@Ashrafs-MacBook-Pro.local>
2018-10-29 00:54:08 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $( "button.navbar-toggler" ).click(function() {
 	$( "body" ).toggleClass("freeze-screen");
   });
+  function myCallback (el) {
+    el.classList.add('is-visible');
+}
+
+ScrollReveal().reveal('.puzzle-piece-1', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-2', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-3', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-4', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-5', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-6', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-7', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-8', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-9', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-10', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-11', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-12', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-13', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-14', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-15', { afterReveal: myCallback });
+ScrollReveal().reveal('.puzzle-piece-16', { afterReveal: myCallback });
